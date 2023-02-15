e319c0c6db4da7b22c26964f3519f8535ca6b38e
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-02-15 21:13:18 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate","12","M","black","sanguine","1"),
+                                                                     ( "Rowdy", "25", "F", "black", "choleric", "1"),
+                                                                     ( "Tabitha", "50", "F", "brown", "sanguine", "1"),
+                                                                     ( "Sergeant Brown", "35", "M", "brown", "phlegmatic", "1"),
+                                                                     ( "Melissa", "15", "F", "white", "phlegmatic", "0"),
+                                                                     ( "Grinch", "15", "M", "black", "choleric", "1"),
+                                                                     ( "Wendy", "15", "F", "black", "melancholic", "1");
+
+INSERT INTO bears (age, sex,color,temperament,alive) VALUES  ("7" , "F", "black","choleric", "1");                                                               
